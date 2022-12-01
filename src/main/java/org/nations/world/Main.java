fd4a3b70f28981c7956bf8684be4e7db0efd08fa
Marc <102824384+EvoX7@users.noreply.github.com>
2022-12-01 10:08:08 +0100

    feat: added 4

--- src/main/java/org/nations/world/Main.java
+++ src/main/java/org/nations/world/Main.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class Main {
 
@@ -13,25 +14,28 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)){
-			
-			 String sql = "SELECT countries.name, countries.country_id, regions.name, continents.name"
-					 + " FROM countries "
-					 + " JOIN regions "
-					 + " ON countries.region_id = regions.region_id "
-					 + " JOIN continents "
-					 + " ON regions.continent_id = continents.continent_id "
-					 + " ORDER BY countries.name ";
-			
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please insert a country to search...");
+		String userInput = sc.nextLine();
+		sc.close();
+
+		try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
+
+			String sql = "SELECT countries.name, countries.country_id, regions.name, continents.name"
+					+ " FROM countries " + " JOIN regions " + " ON countries.region_id = regions.region_id "
+					+ " JOIN continents " + " ON regions.continent_id = continents.continent_id "
+					+ " WHERE countries.name LIKE ? " + " ORDER BY countries.name ";
+
 			try (PreparedStatement ps = con.prepareStatement(sql)) {
+				ps.setString(1, "%" + userInput + "%");
 				try (ResultSet rs = ps.executeQuery()) {
-					while(rs.next()) {
+					while (rs.next()) {
 						final String countries = rs.getString(1);
 						final int id = rs.getInt(2);
 						final String regions = rs.getString(3);
 						final String continents = rs.getString(4);
-						
-						System.out.println(id + " - " + countries + " - " + regions + " - " + continents);
+
+						System.out.println(id + " - " + countries + " | " + regions + " | " + continents);
 					}
 				}
 			}
